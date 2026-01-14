1acfe1180446d45c84d308f8715f59a07806da5d
mohammed gouse <gousemohammed163@gmail.com>
2026-01-14 13:19:10 +0530

    Add ORDER BY clause to retrive the data

--- 01_SQL_Query_SELECT.sql
+++ 01_SQL_Query_SELECT.sql
@@ -41,3 +41,42 @@ SELECT
     country
 FROM customers
 WHERE country = 'Germany';
+
+/* ==============================================================================
+   ORDER BY
+=============================================================================== */
+
+/* Retrieve all customers and 
+   sort the results by the highest score first. */
+SELECT *
+FROM customers
+ORDER BY score DESC;
+
+/* Retrieve all customers and 
+   sort the results by the lowest score first. */
+SELECT *
+FROM customers
+ORDER BY score ASC; /* using ASC or not is optional it is by default ascending order */
+
+/* Retrieve all customers and 
+   sort the results by the country. */
+SELECT *
+FROM customers
+ORDER BY country;
+
+/* Retrieve all customers and 
+   sort the results by the country and then by the highest score. */
+SELECT *
+FROM customers
+ORDER BY country, score DESC;
+
+/* Retrieve the name, country, and score of customers 
+   whose score is not equal to 0
+   and sort the results by the highest score first. */
+SELECT 
+	first_name,
+    country,
+    score
+FROM customers
+WHERE score != 0
+ORDER BY score DESC;
