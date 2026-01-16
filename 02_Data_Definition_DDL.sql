5e4b01e82efb74df2d2e0e42da0ba473a03537cd
mohammed gouse <gousemohammed163@gmail.com>
2026-01-16 11:11:56 +0530

    Add DDL Create to create a table

---
+++ 02_Data_Definition_DDL.sql
@@ -0,0 +1,21 @@
+/*   Table of Contents:
+     1. CREATE - Creating Tables
+     2. ALTER - Modifying Table Structure
+     3. DROP - Removing Tables
+=================================================================================
+*/
+
+/* ============================================================================== 
+   CREATE
+=============================================================================== */
+
+/* Create a new table called persons 
+   with columns: id, person_name, birth_date, and phone */
+CREATE TABLE persons (
+    id INT NOT NULL,
+    person_name VARCHAR(50) NOT NULL,
+    birth_date DATE,
+    phone VARCHAR(15) NOT NULL,
+    PRIMARY KEY (id)
+);
+SELECT * FROM persons;
