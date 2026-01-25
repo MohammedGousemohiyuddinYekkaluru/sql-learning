f9e6357ec24eaa3960ed78747e31ee58be5b509c
mohammed gouse <gousemohammed163@gmail.com>
2026-01-25 11:14:10 +0530

    Add alternative for right anti join and inner join using left join

--- 05_JOINs.sql
+++ 05_JOINs.sql
@@ -122,6 +122,23 @@ RIGHT JOIN orders AS o
 ON c.id = o.customer_id
 WHERE c.id IS NULL;
 
+-- Alternative to RIGHT ANTI JOIN using LEFT JOIN
+/* Get all orders without matching customers */
+SELECT *
+FROM orders AS o
+LEFT JOIN customers AS c
+ON c.id = o.customer_id
+WHERE c.id IS NULL;
+
+-- Alternative to INNER JOIN using LEFT JOIN
+/* Get all customers along with their orders, 
+   but only for customers who have placed an order */
+SELECT *
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NOT NULL;
+
 
 
 
