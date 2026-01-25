e048fa02192eea2e50e17812659e439e071a8de7
mohammed gouse <gousemohammed163@gmail.com>
2026-01-25 11:10:14 +0530

    Add LEFT and RIGHT ANTI JOIN

--- 05_JOINs.sql
+++ 05_JOINs.sql
@@ -102,3 +102,29 @@ FROM customers AS c
 RIGHT JOIN orders AS o
 ON c.id = o.customer_id;
 
+/* ============================================================================== 
+   ADVANCED JOINS
+=============================================================================== */
+
+-- LEFT ANTI JOIN
+/* Get all customers who haven't placed any order */
+SELECT *
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL;
+
+-- RIGHT ANTI JOIN
+/* Get all orders without matching customers */
+SELECT *
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE c.id IS NULL;
+
+
+
+
+
+
+
