14c6d850a10bcf7695bac6b3141b45ae93b8c23d
mohammed gouse <gousemohammed163@gmail.com>
2026-01-31 12:10:18 +0530

    Add difference between NULL, empty strings and black spaces

--- 11_NULL_Functions.sql
+++ 11_NULL_Functions.sql
@@ -111,3 +111,24 @@ FROM Sales.Customers AS c
 LEFT JOIN Sales.Orders AS o
     ON c.CustomerID = o.CustomerID
 WHERE o.CustomerID IS NULL;
+
+/* ==============================================================================
+   NULLs vs EMPTY STRING vs BLANK SPACES
+===============================================================================*/
+
+/* TASK 8: 
+   Demonstrate differences between NULL, empty strings, and blank spaces 
+*/
+WITH Orders AS (
+    SELECT 1 AS Id, 'A' AS Category UNION
+    SELECT 2, NULL UNION
+    SELECT 3, '' UNION
+    SELECT 4, '  '
+)
+SELECT 
+    *,
+    DATALENGTH(Category) AS LenCategory,
+    TRIM(Category) AS Policy1,
+    NULLIF(TRIM(Category), '') AS Policy2,
+    COALESCE(NULLIF(TRIM(Category), ''), 'unknown') AS Policy3
+FROM Orders;
