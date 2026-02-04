d493d6f1845d0b0841a2a0a6359e2451ce7f1010
mohammed gouse <gousemohammed163@gmail.com>
2026-02-04 13:10:57 +0530

    Add Conditional Aggregation

--- 12_Case_Statements.sql
+++ 12_Case_Statements.sql
@@ -109,3 +109,22 @@ SELECT
     ) OVER () AS AvgCustomerClean,
     AVG(Score) OVER () AS AvgCustomer
 FROM Sales.Customers;
+
+/* ==============================================================================
+   CONDITIONAL AGGREGATION
+===============================================================================*/
+
+/* TASK 5: 
+   Count how many orders each customer made with sales greater than 30 
+*/
+SELECT
+    CustomerID,
+    SUM(
+        CASE
+            WHEN Sales > 30 THEN 1
+            ELSE 0
+        END
+    ) AS TotalOrdersHighSales,
+    COUNT(*) AS TotalOrders
+FROM Sales.Orders
+GROUP BY CustomerID;
