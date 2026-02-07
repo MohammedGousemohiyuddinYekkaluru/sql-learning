5d3650f021f819f0425e52e7890918dd7b9ace04
mohammed gouse <gousemohammed163@gmail.com>
2026-02-07 11:51:11 +0530

    Add Aggregate Function using AVERAGE

--- 15_Window_Aggregations.sql
+++ 15_Window_Aggregations.sql
@@ -83,3 +83,47 @@ SELECT
     SUM(Sales) OVER () AS TotalSales,
     ROUND(CAST(Sales AS FLOAT) / SUM(Sales) OVER () * 100, 2) AS PercentageOfTotal
 FROM Sales.Orders;
+
+/* ============================================================
+   SQL WINDOW AGGREGATION | AVG
+   ============================================================ */
+
+/* TASK 6:
+   - Find the Average Sales Across All Orders 
+   - Find the Average Sales for Each Product
+*/
+SELECT
+    OrderID,
+    OrderDate,
+    Sales,
+    ProductID,
+    AVG(Sales) OVER () AS AvgSales,
+    AVG(Sales) OVER (PARTITION BY ProductID) AS AvgSalesByProduct
+FROM Sales.Orders;
+
+/* TASK 7:
+   Find the Average Scores of Customers
+*/
+SELECT
+    CustomerID,
+    LastName,
+    Score,
+    COALESCE(Score, 0) AS CustomerScore,
+    AVG(Score) OVER () AS AvgScore,
+    AVG(COALESCE(Score, 0)) OVER () AS AvgScoreWithoutNull
+FROM Sales.Customers;
+
+/* TASK 8:
+   Find all orders where Sales exceed the average Sales across all orders
+*/
+SELECT
+    *
+FROM (
+    SELECT
+        OrderID,
+        ProductID,
+        Sales,
+        AVG(Sales) OVER () AS Avg_Sales
+    FROM Sales.Orders
+) t 
+WHERE Sales > Avg_Sales;
