178e7132265b15a1d8a03291d1efe330da3a73ff
mohammed gouse <gousemohammed163@gmail.com>
2026-02-07 11:47:38 +0530

    Add Aggregation Function using COUNT

---
+++ 15_Window_Aggregations.sql
@@ -0,0 +1,57 @@
+/* ==============================================================================
+   SQL Window Aggregate Functions
+-------------------------------------------------------------------------------
+   These functions allow you to perform aggregate calculations over a set 
+   of rows without the need for complex subqueries. They enable you to compute 
+   counts, sums, averages, minimums, and maximums while still retaining access 
+   to individual row details.
+
+   Table of Contents:
+    1. COUNT
+    2. SUM
+    3. AVG
+    4. MAX / MIN
+    5. ROLLING SUM & AVERAGE Use Case
+===============================================================================
+*/
+
+/* ============================================================
+   SQL WINDOW AGGREGATION | COUNT
+   ============================================================ */
+
+/* TASK 1:
+   Find the Total Number of Orders and the Total Number of Orders for Each Customer
+*/
+SELECT
+    OrderID,
+    OrderDate,
+    CustomerID,
+    COUNT(*) OVER() AS TotalOrders,
+    COUNT(*) OVER(PARTITION BY CustomerID) AS OrdersByCustomers
+FROM Sales.Orders;
+
+/* TASK 2:
+   - Find the Total Number of Customers
+   - Find the Total Number of Scores for Customers
+   - Find the Total Number of Countries
+*/
+SELECT
+    *,
+    COUNT(*) OVER () AS TotalCustomersStar,
+    COUNT(1) OVER () AS TotalCustomersOne,
+    COUNT(Score) OVER() AS TotalScores,
+    COUNT(Country) OVER() AS TotalCountries
+FROM Sales.Customers;
+
+/* TASK 3:
+   Check whether the table 'OrdersArchive' contains any duplicate rows
+*/
+SELECT 
+    * 
+FROM (
+    SELECT 
+        *,
+        COUNT(*) OVER(PARTITION BY OrderID) AS CheckDuplicates
+    FROM Sales.OrdersArchive
+) t
+WHERE CheckDuplicates > 1;
