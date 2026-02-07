0061d1608da4746f8ab4e15b3a3995d17eaa55b3
mohammed gouse <gousemohammed163@gmail.com>
2026-02-07 11:53:07 +0530

    Add Aggregation Function using MAX / MIN

--- 15_Window_Aggregations.sql
+++ 15_Window_Aggregations.sql
@@ -127,3 +127,52 @@ FROM (
     FROM Sales.Orders
 ) t 
 WHERE Sales > Avg_Sales;
+
+/* ============================================================
+   SQL WINDOW AGGREGATION | MAX / MIN
+   ============================================================ */
+
+/* TASK 9:
+   Find the Highest and Lowest Sales across all orders
+*/
+SELECT 
+    MIN(Sales) AS MinSales, 
+    MAX(Sales) AS MaxSales 
+FROM Sales.Orders;
+
+/* TASK 10:
+   Find the Lowest Sales across all orders and by Product
+*/
+SELECT 
+    OrderID,
+    ProductID,
+    OrderDate,
+    Sales,
+    MIN(Sales) OVER () AS LowestSales,
+    MIN(Sales) OVER (PARTITION BY ProductID) AS LowestSalesByProduct
+FROM Sales.Orders;
+
+/* TASK 11:
+   Show the employees who have the highest salaries
+*/
+SELECT *
+FROM (
+	SELECT *,
+		   MAX(Salary) OVER() AS HighestSalary
+	FROM Sales.Employees
+) t
+WHERE Salary = HighestSalary;
+
+/* TASK 12:
+   Find the deviation of each Sale from the minimum and maximum Sales
+*/
+SELECT
+    OrderID,
+    OrderDate,
+    ProductID,
+    Sales,
+    MAX(Sales) OVER () AS HighestSales,
+    MIN(Sales) OVER () AS LowestSales,
+    Sales - MIN(Sales) OVER () AS DeviationFromMin,
+    MAX(Sales) OVER () - Sales AS DeviationFromMax
+FROM Sales.Orders;
