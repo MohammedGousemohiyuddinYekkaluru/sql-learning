750af9189fb485156980932792c630ae01cdb620
mohammed gouse <gousemohammed163@gmail.com>
2026-02-08 17:42:04 +0530

    Add NILTE in window ranking

--- 16_Window_Ranking.sql
+++ 16_Window_Ranking.sql
@@ -79,3 +79,48 @@ FROM (
     FROM Sales.OrdersArchive
 ) AS UniqueOrdersArchive
 WHERE rn = 1;
+
+/* ============================================================
+   SQL WINDOW RANKING | NTILE
+   ============================================================ */
+
+/* TASK 6:
+   Divide Orders into Groups Based on Sales
+*/
+SELECT 
+    OrderID,
+    Sales,
+    NTILE(1) OVER (ORDER BY Sales) AS OneBucket,
+    NTILE(2) OVER (ORDER BY Sales) AS TwoBuckets,
+    NTILE(3) OVER (ORDER BY Sales) AS ThreeBuckets,
+    NTILE(4) OVER (ORDER BY Sales) AS FourBuckets,
+    NTILE(2) OVER (PARTITION BY ProductID ORDER BY Sales) AS TwoBucketByProducts
+FROM Sales.Orders;
+
+/* TASK 7:
+   Segment all Orders into 3 Categories: High, Medium, and Low Sales.
+*/
+SELECT
+    OrderID,
+    Sales,
+    Buckets,
+    CASE 
+        WHEN Buckets = 1 THEN 'High'
+        WHEN Buckets = 2 THEN 'Medium'
+        WHEN Buckets = 3 THEN 'Low'
+    END AS SalesSegmentations
+FROM (
+    SELECT
+        OrderID,
+        Sales,
+        NTILE(3) OVER (ORDER BY Sales DESC) AS Buckets
+    FROM Sales.Orders
+) AS SalesBuckets;
+
+/* TASK 8:
+   Divide Orders into Groups for Processing
+*/
+SELECT 
+    NTILE(5) OVER (ORDER BY OrderID) AS Buckets,
+    *
+FROM Sales.Orders;
