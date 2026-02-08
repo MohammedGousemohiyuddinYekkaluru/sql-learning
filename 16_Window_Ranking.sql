ca08104e6951d4aac8dc240f6969242a2985167e
mohammed gouse <gousemohammed163@gmail.com>
2026-02-08 17:40:37 +0530

    Add ROW_NUMBER, RANK & DENSE_RANK

---
+++ 16_Window_Ranking.sql
@@ -0,0 +1,81 @@
+/* ==============================================================================
+   SQL Window Ranking Functions
+-------------------------------------------------------------------------------
+   These functions allow you to rank and order rows within a result set 
+   without the need for complex joins or subqueries. They enable you to assign 
+   unique or non-unique rankings, group rows into buckets, and analyze data 
+   distributions on ordered data.
+
+   Table of Contents:
+     1. ROW_NUMBER
+     2. RANK
+     3. DENSE_RANK
+     4. NTILE
+     5. CUME_DIST
+=================================================================================
+*/
+
+/* ============================================================
+   SQL WINDOW RANKING | ROW_NUMBER, RANK, DENSE_RANK
+   ============================================================ */
+
+/* TASK 1:
+   Rank Orders Based on Sales from Highest to Lowest
+*/
+SELECT
+    OrderID,
+    ProductID,
+    Sales,
+    ROW_NUMBER() OVER (ORDER BY Sales DESC) AS SalesRank_Row,
+    RANK() OVER (ORDER BY Sales DESC) AS SalesRank_Rank,
+    DENSE_RANK() OVER (ORDER BY Sales DESC) AS SalesRank_Dense
+FROM Sales.Orders;
+
+/* TASK 2:
+   Use Case | Top-N Analysis: Find the Highest Sale for Each Product
+*/
+SELECT *
+FROM (
+    SELECT
+        OrderID,
+        ProductID,
+        Sales,
+        ROW_NUMBER() OVER (PARTITION BY ProductID ORDER BY Sales DESC) AS RankByProduct
+    FROM Sales.Orders
+) AS TopProductSales
+WHERE RankByProduct = 1;
+
+/* TASK 3:
+   Use Case | Bottom-N Analysis: Find the Lowest 2 Customers Based on Their Total Sales
+*/
+SELECT *
+FROM (
+    SELECT
+        CustomerID,
+        SUM(Sales) AS TotalSales,
+        ROW_NUMBER() OVER (ORDER BY SUM(Sales)) AS RankCustomers
+    FROM Sales.Orders
+    GROUP BY CustomerID
+) AS BottomCustomerSales
+WHERE RankCustomers <= 2;
+
+/* TASK 4:
+   Use Case | Assign Unique IDs to the Rows of the 'Order Archive'
+*/
+SELECT
+    ROW_NUMBER() OVER (ORDER BY OrderID, OrderDate) AS UniqueID,
+    *
+FROM Sales.OrdersArchive;
+
+/* TASK 5:
+   Use Case | Identify Duplicates:
+   Identify Duplicate Rows in 'Order Archive' and return a clean result without any duplicates
+*/
+SELECT *
+FROM (
+    SELECT
+        ROW_NUMBER() OVER (PARTITION BY OrderID ORDER BY CreationTime DESC) AS rn,
+        *
+    FROM Sales.OrdersArchive
+) AS UniqueOrdersArchive
+WHERE rn = 1;
