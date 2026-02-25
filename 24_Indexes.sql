fffbaec0c6f93240460260091f7cb5906bd937ac
mohammed gouse <gousemohammed163@gmail.com>
2026-02-25 12:43:58 +0530

    Add Fragmentations

--- 24_Indexes.sql
+++ 24_Indexes.sql
@@ -255,3 +255,31 @@ GO
 -- Update statistics for all tables in the database
 EXEC sp_updatestats;
 GO
+
+/* ==============================================================================
+   Fragementations
+============================================================================== */
+
+-- Retrieve index fragmentation statistics for the current database
+SELECT 
+    tbl.name AS TableName,
+    idx.name AS IndexName,
+    s.avg_fragmentation_in_percent,
+    s.page_count
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS s
+INNER JOIN sys.tables tbl 
+    ON s.object_id = tbl.object_id
+INNER JOIN sys.indexes AS idx 
+    ON idx.object_id = s.object_id
+    AND idx.index_id = s.index_id
+ORDER BY s.avg_fragmentation_in_percent DESC;
+
+-- Reorganize the index (lightweight defragmentation)
+ALTER INDEX idx_Customers_CS_Country 
+ON Sales.Customers REORGANIZE;
+GO
+
+-- Rebuild the index (full rebuild, more resource-intensive)
+ALTER INDEX idx_Customers_Country 
+ON Sales.Customers REBUILD;
+GO
