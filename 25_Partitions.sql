e5182c3dc9d8c7d9803c925ea68096a60b68993d
mohammed gouse <gousemohammed163@gmail.com>
2026-02-26 19:05:27 +0530

    Query: List All Existing Files in SalesDB

--- 25_Partitions.sql
+++ 25_Partitions.sql
@@ -80,3 +80,16 @@ ALTER DATABASE SalesDB ADD FILE
 	NAME = P_2026, -- Logical Name
 	FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\P_2026.ndf'
 ) TO FILEGROUP FG_2026;
+
+-- Query: List All Existing Files in SalesDB
+SELECT 
+    fg.name AS FilegroupName,
+    mf.name AS LogicalFileName,
+    mf.physical_name AS PhysicalFilePath,
+    mf.size / 128 AS SizeInMB
+FROM 
+    sys.filegroups fg
+JOIN 
+    sys.master_files mf ON fg.data_space_id = mf.data_space_id
+WHERE 
+    mf.database_id = DB_ID('SalesDB')
