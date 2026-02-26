d56d3ff427155ae11eec116c694f2af01514bd7d
mohammed gouse <gousemohammed163@gmail.com>
2026-02-26 19:02:50 +0530

    Create Filegroups

--- 25_Partitions.sql
+++ 25_Partitions.sql
@@ -33,3 +33,21 @@ SELECT
 	type_desc,
 	boundary_value_on_right
 FROM sys.partition_functions
+
+/* ==============================================================================
+   Step 2: Create Filegroups
+============================================================================== */
+
+-- Create Filegroups in SalesDB
+ALTER DATABASE SalesDB ADD FILEGROUP FG_2023;
+ALTER DATABASE SalesDB ADD FILEGROUP FG_2024;
+ALTER DATABASE SalesDB ADD FILEGROUP FG_2025;
+ALTER DATABASE SalesDB ADD FILEGROUP FG_2026;
+
+-- Optional: Remove a Filegroup if needed
+ALTER DATABASE SalesDB REMOVE FILEGROUP FG_2023;
+
+-- Query: List All Existing Filegroups (filter by name pattern if needed)
+SELECT *
+FROM sys.filegroups
+WHERE type = 'FG'
