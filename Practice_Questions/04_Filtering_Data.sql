48b56260396810bf4bd426222526de2d5982b572
mohammed gouse <gousemohammed163@gmail.com>
2026-03-21 21:52:45 +0530

    Find all books that are currently 'Available' AND have a times_rented greater than 40.

--- Practice_Questions/04_Filtering_Data.sql
+++ Practice_Questions/04_Filtering_Data.sql
@@ -5,3 +5,12 @@ Find all books that have been rented more than 20 times but less than 50 times.
 SELECT *
 FROM Books
 WHERE times_rented > 20 AND times_rented < 50;
+
+/*
+We need a report on books that are high priority.
+Find all books that are currently 'Available' AND have a times_rented greater than 40.
+*/
+
+SELECT * 
+FROM Books
+WHERE status = 'Available' AND times_rented > 40;
