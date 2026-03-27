3309168c9830f19d5bbfbaeb80fb3fdcb4123e88
mohammed gouse <gousemohammed163@gmail.com>
2026-03-27 16:37:01 +0530

    Create a Display Name for every book.

--- Practice_Questions/07_String_Functions.sql
+++ Practice_Questions/07_String_Functions.sql
@@ -4,3 +4,15 @@ VALUES
     (20, '   THE GREAT GATSBY   ', 'f. scott fitzgerald', 'Available', 0),
     (21, 'Harry Potter: Part 1', 'J.K. Rowling', 'Available', 10),
     (22, 'clean_code_book', 'Robert Martin', 'Available', 5);
+
+
+/*
+Create a "Display Name" for every book.
+Combine the title and the author into one column called Book_Info.
+Make the Title all UPPERCASE and the Author all lowercase.
+Example Output: THE HOBBIT - j.r.r. tolkien
+*/
+
+SELECT 
+    CONCAT(UPPER(title), ' - ' , LOWER(author)) AS Book_Info
+FROM Books;
