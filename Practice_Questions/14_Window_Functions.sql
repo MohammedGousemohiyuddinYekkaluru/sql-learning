227572a226f2a0eebf5ba3b6bc64bb46127f34d1
mohammed gouse <gousemohammed163@gmail.com>
2026-04-30 13:12:52 +0530

    Solve task 1 that shows 'Grand Total'

---
+++ Practice_Questions/14_Window_Functions.sql
@@ -0,0 +1,16 @@
+select * from Products
+select * from Sales
+
+/*
+Task:1 :- The "Grand Total" Comparison
+Management wants to see how much each sale contributes to the company's overall success.
+Show: sale_id, store_location, quantity_sold, and a new column Grand_Total_Sold.
+Grand_Total_Sold should show the sum of all quantity_sold in the entire table, repeated on every row.
+*/
+
+SELECT 
+	sale_id,
+	store_location,
+	quantity_sold,
+	SUM(quantity_sold) OVER () Grand_Total_Sold
+FROM Sales;
