4f0ec9618f5f0dc6ec7da20c7f1b270ae61a125f
mohammed gouse <gousemohammed163@gmail.com>
2026-05-10 18:21:10 +0530

    Solve Task 2: Store's First Product

--- Practice_Questions/17_Window_Value_Functions.sql
+++ Practice_Questions/17_Window_Value_Functions.sql
@@ -13,3 +13,15 @@ SELECT
 	LAG(quantity_sold) OVER(ORDER BY sale_date) Previous_Sale_Quantity,
 	quantity_sold - LAG(quantity_sold) OVER(ORDER BY sale_date) Quantity_sale_difference
 FROM Sales;
+
+/*
+Task 2: Store's First Product
+Management wants to know which product was the very first one sold in each store.
+*/
+
+SELECT
+	store_location,
+	sale_date,
+	product_id,
+	FIRST_VALUE(product_id) OVER(PARTITION BY store_location ORDER BY sale_date) First_Product_Sold
+FROM Sales;
